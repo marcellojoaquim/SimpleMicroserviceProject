1a89c2ad0b22a9488eab5920638138f4e4efd034
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-10-16 19:28:34 -0300

    configuracao do service discovery com eureka client

--- ProdutoService/src/main/java/com/mjdsilva/produto/service/ProdutoServiceApplication.java
+++ ProdutoService/src/main/java/com/mjdsilva/produto/service/ProdutoServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class ProdutoServiceApplication {
 
 	public static void main(String[] args) {
--- VendasOnline/src/main/java/com/mjsilva/vendas/VendasOnlineApplication.java
+++ VendasOnline/src/main/java/com/mjsilva/vendas/VendasOnlineApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @RefreshScope
 @EnableFeignClients
 @EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class VendasOnlineApplication {
 
 	public static void main(String[] args) {
